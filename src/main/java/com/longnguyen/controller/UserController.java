1bbfd228f0197f454b98cd6fa2eda85c0a9b5a5f
long <fchbyoung1010@gmail.com>
2019-08-15 15:17:01 +0700

    complete

--- src/main/java/com/longnguyen/controller/UserController.java
+++ src/main/java/com/longnguyen/controller/UserController.java
@@ -1,9 +1,31 @@
 package com.longnguyen.controller;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class UserController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user(Principal principal) {
+        //Get authenticated user name from Principal
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        //Get authenticated user name from SecurityContext
+        SecurityContext context = SecurityContextHolder.getContext();
+        return "admin";
+    }
 
 }
